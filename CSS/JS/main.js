e30cba309813c189b72e3afeb3e75ac717649cab
Michelle A Limon <mlimon0789@gmail.com>
2020-11-21 17:30:53 -0600

    Lots of updates

--- CSS/JS/main.js
+++ CSS/JS/main.js
@@ -1,46 +1,13 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 
-    
-    $('#search-button').on('click', () => {
-     
-      let searchTerm = $('#search-field').val();
-
-      console.log(searchTerm);
-
-      let url = "https://api.giphy.com/v1/gifs/search?api_key=4zv45wmD5vJvLn2UE3B4tNBY1ny9kZZV&q=" + searchTerm;
-
-
-      // jquery way of doin it
-      $.get(url, function(giphyResponse) {
-        
-// 
-
-        let resultsHtml = '';
-
-
-        
-
-
-        for (let image of giphyResponse.data) {                
-          // let imgHtml = `<img src=${image.images.original.url} class="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2 col-span-12 sm:col-span-6 md:col-span-4 "/>`
-          let imgHtml = `<img src=${image.images.original.url} />`
-          resultsHtml = resultsHtml + imgHtml;          
-        }
-
-        console.log(resultsHtml);
-
-        $('#search-results').html(resultsHtml);
-
-
-      });
-
-
-
-
+    $('#fetch-from-api').on('click', (} => {
+        console.log('I was clicked!');
+        $.get('api.giphy.com/v1/gifs/search')
 
     })
 
 
 
-});
+
+});  
