02ccd5ce41afd3a6d90f41e09def568563c24c32
Michelle A Limon <mlimon0789@gmail.com>
2020-11-21 18:10:51 -0600

    Trying something new

--- CSS/JS/main.js
+++ CSS/JS/main.js
@@ -1,12 +1,28 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 
-    $('#fetch-from-api').on('click', (} => {
-        console.log('I was clicked!');
-        $.get('api.giphy.com/v1/gifs/search')
+    $('#search-button').on('click', () => {
+        let searchTerm = $('#search-field').val();
+
+        console.log(searchTerm);
+
+        $.get('https://api.giphy.com/v1/gifs/search?api_key=VT3E7s35IVGNTJL23r5PgMJQjp4u3ydt&q={searchTerm}', function(giphyResponse) {
+            console.log(giphyResponse);
+            console.log('giphyData', giphyResponse.data)
+            console.log('giphyData result 0 (1st result)', giphyResponse.data[0]);
+            console.log('giphyData result 0 (1st result') - all images list', giphyResponse.data[0].images);
+            console.log('giphyData result 0 (1st result) - all images list - original image', giphyResponse.data[0].images.original);
+
+            console.log('giphyData result 0 (1st result) - all images list - original image - url', giphyResponse.data[0].images.original.url);
+
+            for (let image of giphyResponse.data) {
+                let imgHtml = '<img src=${image.images.original.url} />'
+                $('#first-result-image').append(imgHtml);
+            }
+        });
+
 
     })
-
 
 
 
