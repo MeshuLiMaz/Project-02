246c5b2ce9368a75b423d727440b1b3da1a3324a
Michelle A Limon <mlimon0789@gmail.com>
2020-11-21 21:51:34 -0600

    working out kinks with jQuery

--- CSS/JS/main.js
+++ CSS/JS/main.js
@@ -6,18 +6,20 @@ $( document ).ready(function() {
 
         console.log(searchTerm);
 
-        $.get('https://api.giphy.com/v1/gifs/search?api_key=VT3E7s35IVGNTJL23r5PgMJQjp4u3ydt&q={searchTerm}', function(giphyResponse) {
+        let url = "https://api.giphy.com/v1/gifs/search?api_key=VT3E7s35IVGNTJL23r5PgMJQjp4u3ydt&q" + searchTerm;
+
+            $.get(url, function(giphyResponse) {
             console.log(giphyResponse);
             console.log('giphyData', giphyResponse.data)
             console.log('giphyData result 0 (1st result)', giphyResponse.data[0]);
-            console.log('giphyData result 0 (1st result') - all images list', giphyResponse.data[0].images);
+            console.log('giphyData result 0 (1st result) - all images list', giphyResponse.data[0].images);
             console.log('giphyData result 0 (1st result) - all images list - original image', giphyResponse.data[0].images.original);
 
             console.log('giphyData result 0 (1st result) - all images list - original image - url', giphyResponse.data[0].images.original.url);
 
             for (let image of giphyResponse.data) {
                 let imgHtml = '<img src=${image.images.original.url} />'
-                $('#first-result-image').append(imgHtml);
+                $('#search-results').append(imgHtml);
             }
         });
 
